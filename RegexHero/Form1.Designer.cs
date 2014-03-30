31612089d9d9e9a89dfe3447c3bf3f46c940bacc
Peter Nied <peternied@hotmail.com>
2014-03-30 16:41:12 -0700

    Basic Event Logic

--- RegexHero/Form1.Designer.cs
+++ RegexHero/Form1.Designer.cs
@@ -31,10 +31,22 @@
             this.dataEntryPanel = new System.Windows.Forms.Panel();
             this.previewPanel = new System.Windows.Forms.Panel();
             this.resultsPanel = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.scoreLabel = new System.Windows.Forms.Label();
+            this.materialsBox = new System.Windows.Forms.RichTextBox();
+            this.dataEntryPanel.SuspendLayout();
+            this.previewPanel.SuspendLayout();
+            this.resultsPanel.SuspendLayout();
             this.SuspendLayout();
             // 
             // dataEntryPanel
             // 
+            this.dataEntryPanel.Controls.Add(this.textBox1);
+            this.dataEntryPanel.Controls.Add(this.linkLabel1);
+            this.dataEntryPanel.Controls.Add(this.label1);
             this.dataEntryPanel.Location = new System.Drawing.Point(13, 12);
             this.dataEntryPanel.Name = "dataEntryPanel";
             this.dataEntryPanel.Size = new System.Drawing.Size(659, 74);
@@ -42,6 +54,7 @@
             // 
             // previewPanel
             // 
+            this.previewPanel.Controls.Add(this.materialsBox);
             this.previewPanel.Location = new System.Drawing.Point(13, 93);
             this.previewPanel.Name = "previewPanel";
             this.previewPanel.Size = new System.Drawing.Size(659, 247);
@@ -49,10 +62,67 @@
             // 
             // resultsPanel
             // 
+            this.resultsPanel.Controls.Add(this.scoreLabel);
+            this.resultsPanel.Controls.Add(this.label2);
             this.resultsPanel.Location = new System.Drawing.Point(13, 346);
             this.resultsPanel.Name = "resultsPanel";
             this.resultsPanel.Size = new System.Drawing.Size(659, 36);
             this.resultsPanel.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(4, 4);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(121, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Express your Expression";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(593, 4);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(63, 13);
+            this.linkLabel1.TabIndex = 1;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "CheatSheet";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.NoCheating);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(7, 21);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(649, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.RegexUpdated);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 4);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Current Score:";
+            // 
+            // scoreLabel
+            // 
+            this.scoreLabel.AutoSize = true;
+            this.scoreLabel.Location = new System.Drawing.Point(88, 4);
+            this.scoreLabel.Name = "scoreLabel";
+            this.scoreLabel.Size = new System.Drawing.Size(35, 13);
+            this.scoreLabel.TabIndex = 1;
+            this.scoreLabel.Text = "label3";
+            // 
+            // materialsBox
+            // 
+            this.materialsBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.materialsBox.Location = new System.Drawing.Point(0, 0);
+            this.materialsBox.Name = "materialsBox";
+            this.materialsBox.Size = new System.Drawing.Size(659, 247);
+            this.materialsBox.TabIndex = 0;
+            this.materialsBox.Text = "";
             // 
             // Form1
             // 
@@ -64,6 +134,11 @@
             this.Controls.Add(this.dataEntryPanel);
             this.Name = "Form1";
             this.Text = "Regex Hero";
+            this.dataEntryPanel.ResumeLayout(false);
+            this.dataEntryPanel.PerformLayout();
+            this.previewPanel.ResumeLayout(false);
+            this.resultsPanel.ResumeLayout(false);
+            this.resultsPanel.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -73,6 +148,12 @@
         private System.Windows.Forms.Panel dataEntryPanel;
         private System.Windows.Forms.Panel previewPanel;
         private System.Windows.Forms.Panel resultsPanel;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label scoreLabel;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox materialsBox;
     }
 }
 
--- RegexHero/Form1.cs
+++ RegexHero/Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,9 +13,56 @@ namespace RegexHero
 {
     public partial class Form1 : Form
     {
+        private event Action<string> UpdateRegex;
+        private event Action<TextBox> BadRegexUpdate;
+        private event Action<Regex, MatchCollection> CalculateScore;
+
         public Form1()
         {
             InitializeComponent();
+
+            this.UpdateRegex += (pattern) =>
+            {
+                Regex r = null;
+                MatchCollection mc = null;
+                try
+                {
+                    r = new Regex(pattern);
+                    mc = r.Matches(this.materialsBox.Text);
+                    
+                } catch(Exception)
+                {
+                    this.BadRegexUpdate(this.textBox1);
+                }
+                
+                this.CalculateScore(r, mc);
+            };
+
+            this.BadRegexUpdate += (textBox) =>
+            {
+                textBox.ForeColor = Color.Red;
+            };
+
+            CalculateScore += (regex, matches) =>
+            {
+                scoreLabel.Text = DateTime.Now.ToLongTimeString();
+            };
+        }
+
+        private void NoCheating(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            MessageBox.Show("No Cheating!");
+        }
+
+        private void RegexUpdated(object sender, EventArgs e)
+        {
+            TextBox source = sender as TextBox;
+            if (source == null)
+            {
+                return;
+            }
+            source.ForeColor = Color.Black;
+            this.UpdateRegex(source.Text);
         }
     }
 }
