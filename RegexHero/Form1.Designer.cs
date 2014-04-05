1510fba0e0c896fe4dabbc096b451685aabaa375
Peter Nied <peternied@hotmail.com>
2014-04-05 13:30:36 -0700

    New high score panel UI

--- RegexHero/Form1.Designer.cs
+++ RegexHero/Form1.Designer.cs
@@ -38,9 +38,15 @@
             this.scoreLabel = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.highScoresTextBox = new System.Windows.Forms.RichTextBox();
+            this.highScoresPanel = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.maxScoreLabel = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label4 = new System.Windows.Forms.Label();
             this.dataEntryPanel.SuspendLayout();
             this.previewPanel.SuspendLayout();
             this.resultsPanel.SuspendLayout();
+            this.highScoresPanel.SuspendLayout();
             this.SuspendLayout();
             // 
             // dataEntryPanel
@@ -49,7 +55,7 @@
             this.dataEntryPanel.Controls.Add(this.linkLabel1);
             this.dataEntryPanel.Controls.Add(this.label1);
             this.dataEntryPanel.Location = new System.Drawing.Point(17, 15);
-            this.dataEntryPanel.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dataEntryPanel.Margin = new System.Windows.Forms.Padding(4);
             this.dataEntryPanel.Name = "dataEntryPanel";
             this.dataEntryPanel.Size = new System.Drawing.Size(879, 91);
             this.dataEntryPanel.TabIndex = 0;
@@ -57,7 +63,7 @@
             // textBox1
             // 
             this.textBox1.Location = new System.Drawing.Point(9, 26);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(864, 22);
             this.textBox1.TabIndex = 2;
@@ -89,7 +95,7 @@
             // 
             this.previewPanel.Controls.Add(this.materialsBox);
             this.previewPanel.Location = new System.Drawing.Point(17, 114);
-            this.previewPanel.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.previewPanel.Margin = new System.Windows.Forms.Padding(4);
             this.previewPanel.Name = "previewPanel";
             this.previewPanel.Size = new System.Drawing.Size(879, 304);
             this.previewPanel.TabIndex = 1;
@@ -98,7 +104,7 @@
             // 
             this.materialsBox.Dock = System.Windows.Forms.DockStyle.Fill;
             this.materialsBox.Location = new System.Drawing.Point(0, 0);
-            this.materialsBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.materialsBox.Margin = new System.Windows.Forms.Padding(4);
             this.materialsBox.Name = "materialsBox";
             this.materialsBox.Size = new System.Drawing.Size(879, 304);
             this.materialsBox.TabIndex = 0;
@@ -106,10 +112,13 @@
             // 
             // resultsPanel
             // 
+            this.resultsPanel.Controls.Add(this.checkBox1);
+            this.resultsPanel.Controls.Add(this.maxScoreLabel);
+            this.resultsPanel.Controls.Add(this.label3);
             this.resultsPanel.Controls.Add(this.scoreLabel);
             this.resultsPanel.Controls.Add(this.label2);
             this.resultsPanel.Location = new System.Drawing.Point(17, 426);
-            this.resultsPanel.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.resultsPanel.Margin = new System.Windows.Forms.Padding(4);
             this.resultsPanel.Name = "resultsPanel";
             this.resultsPanel.Size = new System.Drawing.Size(879, 44);
             this.resultsPanel.TabIndex = 2;
@@ -120,8 +129,9 @@
             this.scoreLabel.Location = new System.Drawing.Point(117, 5);
             this.scoreLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.scoreLabel.Name = "scoreLabel";
-            this.scoreLabel.Size = new System.Drawing.Size(0, 17);
+            this.scoreLabel.Size = new System.Drawing.Size(16, 17);
             this.scoreLabel.TabIndex = 1;
+            this.scoreLabel.Text = "0";
             // 
             // label2
             // 
@@ -135,22 +145,70 @@
             // 
             // highScoresTextBox
             // 
-            this.highScoresTextBox.Location = new System.Drawing.Point(904, 118);
+            this.highScoresTextBox.Location = new System.Drawing.Point(3, 26);
             this.highScoresTextBox.Name = "highScoresTextBox";
-            this.highScoresTextBox.Size = new System.Drawing.Size(146, 352);
+            this.highScoresTextBox.Size = new System.Drawing.Size(146, 426);
             this.highScoresTextBox.TabIndex = 3;
             this.highScoresTextBox.Text = "";
+            // 
+            // highScoresPanel
+            // 
+            this.highScoresPanel.Controls.Add(this.label4);
+            this.highScoresPanel.Controls.Add(this.highScoresTextBox);
+            this.highScoresPanel.Location = new System.Drawing.Point(904, 15);
+            this.highScoresPanel.Name = "highScoresPanel";
+            this.highScoresPanel.Size = new System.Drawing.Size(146, 455);
+            this.highScoresPanel.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(184, 5);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(82, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "High Score:";
+            // 
+            // maxScoreLabel
+            // 
+            this.maxScoreLabel.AutoSize = true;
+            this.maxScoreLabel.Location = new System.Drawing.Point(269, 5);
+            this.maxScoreLabel.Name = "maxScoreLabel";
+            this.maxScoreLabel.Size = new System.Drawing.Size(16, 17);
+            this.maxScoreLabel.TabIndex = 3;
+            this.maxScoreLabel.Text = "0";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Location = new System.Drawing.Point(732, 5);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(141, 21);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = "Show HighScores";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(4, 5);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(120, 17);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Daily High Scores";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1062, 485);
-            this.Controls.Add(this.highScoresTextBox);
+            this.Controls.Add(this.highScoresPanel);
             this.Controls.Add(this.resultsPanel);
             this.Controls.Add(this.previewPanel);
             this.Controls.Add(this.dataEntryPanel);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Text = "Regex Hero";
             this.dataEntryPanel.ResumeLayout(false);
@@ -158,6 +216,8 @@
             this.previewPanel.ResumeLayout(false);
             this.resultsPanel.ResumeLayout(false);
             this.resultsPanel.PerformLayout();
+            this.highScoresPanel.ResumeLayout(false);
+            this.highScoresPanel.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -174,6 +234,11 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.RichTextBox materialsBox;
         private System.Windows.Forms.RichTextBox highScoresTextBox;
+        private System.Windows.Forms.Label maxScoreLabel;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel highScoresPanel;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label4;
     }
 }
 
